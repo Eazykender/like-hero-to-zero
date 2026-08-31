bacbf5ea97198e328cc1d33d50643eef6767dbc4
Eazykender <iskender-dumlu@hotmail.de>
2026-08-31 14:41:04 +0200

    Hibernate- und MySQL-Persistenz eingerichtet

---
+++ src/main/java/de/iu/likeherotozero/persistence/PersistenceManager.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package de.iu.likeherotozero.persistence;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.Initialized;
+import jakarta.enterprise.event.Observes;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
+import jakarta.annotation.PreDestroy;
+import jakarta.persistence.EntityManager;
+/**
+ *
+ * @author Iskender Dumlu
+ */
+@ApplicationScoped
+public class PersistenceManager {
+    
+    private EntityManagerFactory entityManagerFactory;
+
+public void initialize(
+        @Observes @Initialized(ApplicationScoped.class) Object event) {
+    String databaseUser = System.getenv("LHTZ_DB_USER");
+    String databasePassword = System.getenv("LHTZ_DB_PASSWORD");
+
+    if (databaseUser == null || databasePassword == null) {
+        throw new IllegalStateException(
+                "Die lokalen Datenbank-Zugangsdaten fehlen.");
+    }
+
+    Map<String, Object> properties = new HashMap<>();
+    properties.put("jakarta.persistence.jdbc.user", databaseUser);
+    properties.put("jakarta.persistence.jdbc.password", databasePassword);
+
+    entityManagerFactory = Persistence.createEntityManagerFactory(
+            "likeHeroToZeroPU", properties);
+}
+public EntityManager createEntityManager() {
+    if (entityManagerFactory == null || !entityManagerFactory.isOpen()) {
+        throw new IllegalStateException(
+                "Die JPA-Verbindung ist nicht verfügbar.");
+    }
+
+    return entityManagerFactory.createEntityManager();
+}
+
+@PreDestroy
+public void close() {
+    if (entityManagerFactory != null && entityManagerFactory.isOpen()) {
+        entityManagerFactory.close();
+    }
+}   
+}
